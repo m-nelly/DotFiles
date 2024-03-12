2a48636f428273dd2cd6e91e0c327b5b9e9322b0
mnelly <mnelly.sec@gmail.com>
2024-03-12 06:20:55 -0500

    reorg and push new files

---
+++ arch/ip.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+interfaces=$(ip -o link show | awk -F': ' '!/lo/ && !/v/ && !/DOWN/ {print $2}')
+
+result=""
+
+for i in $interfaces; do 
+	address=$(ip -o -4 addr show $i | awk '{print $4}' | cut -d '/' -f 1)
+	
+	case $i in
+		e*) i="󰈀 " ;;
+		w*) i=" " ;;
+		t*) i="󰖂 " ;;
+	esac
+
+	result="$result$i$address "
+done
+
+echo $result

5ca91bd5549b1df6d1a5e4f00efb311884e0e20c
m-nelly <mnelly.sec@protonmail.com>
2025-04-19 13:30:31 -0500

    cleanup

--- rofi/rofi-emoji/src/formatter.h
+++
@@ -1,8 +0,0 @@
-#ifndef FORMATTER_H
-#define FORMATTER_H
-
-#include "emoji.h"
-
-char *format_emoji(const Emoji *emoji, const char *format);
-
-#endif // FORMATTER_H
--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo mkdir -p /usr/share/zsh/plugins
-sudo chown $USER /usr/share/zsh/plugins 
-
-while read plugin; do
-	git clone https://github.com/$plugin.git /usr/share/zsh/plugins/$plugin
-done < ~/DotFiles/zsh-plugins.lst
-
